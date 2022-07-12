31c58ba43ae7a28b978272be5f4faf6743a755cf
Iskander <iskander-m.s@mail.ru>
2022-07-13 00:45:14 +0700

    Задача 1 решена

--- Task_01/Program.cs
+++ Task_01/Program.cs
@@ -1,15 +1,14 @@
-﻿// 1. Напишите программу, которая на вход принимаетдва числа и проверяет,
-// является ли первое число квадратом второго
+﻿// 1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго
 
-Console.Write("Введите два целых числа: ");
+Console.WriteLine("Введите первое целое число и нажмите Enter \nВведите второе целое число и нажмите Enter ");
 int num_A = Convert.ToInt32(Console.ReadLine());
 int num_B = Convert.ToInt32(Console.ReadLine());
-if(num_A / num_B = num_B)
+if(num_A / num_B == num_B)
 {
-    Console,WriteLine($"Число {num_A} является квадратом числа {num_B}");
+    Console.WriteLine($"Число {num_A} является квадратом числа {num_B}");
 }
 else
 {
-    Console,WriteLine($"Число {num_A} не является квадратом числа {num_B}");
+    Console.WriteLine($"Число {num_A} не является квадратом числа {num_B}");
 }
 
