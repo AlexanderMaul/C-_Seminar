a8be852c283f26ca0e10a67dc647f311e4652d57
Iskander <iskander-m.s@mail.ru>
2022-07-13 01:43:26 +0700

    Задача 3 усовершенствована

--- Task_03/Program.cs
+++ Task_03/Program.cs
@@ -2,53 +2,65 @@
 
 Console.WriteLine("Введите число от 1 до 7: ");
 int num = Convert.ToInt32(Console.ReadLine());
-if(num == 1)
-{
-    Console.WriteLine("Понедельник - это первый день недели!");
-}
-else
-{
-    if(num == 2)
-    {
-        Console.WriteLine("Вторник - это второй день недели!");
-    }
-    else
-    {
-        if(num == 3)
-        {
-            Console.WriteLine("Среда - это третий день недели!");
-        }
-        else
-        {
-            if(num == 4)
-            {
-                Console.WriteLine("Четверг - это четвёртый день недели!");
-            }
-            else
-            {
-                if(num == 5)
-                {
-                    Console.WriteLine("Пятница - это пятый день недели!");
-                }
-                else
-                {
-                    if(num == 6)
-                    {
-                        Console.WriteLine("Суббота - это шестой день недели!");
-                    }
-                    else{
-                        if(num == 7)
-                        {
-                            Console.WriteLine("Воскресенье - это седьмой день недели!");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Какое большое число - в неделе всего семь дней;)");
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
+
+if(num == 1) Console.WriteLine("Понедельник - это первый день недели!");
+else if(num == 2) Console.WriteLine("Вторник - это второй день недели!");
+else if(num == 3) Console.WriteLine("Среда - это третий день недели!");
+else if(num == 4) Console.WriteLine("Четверг - это четвёртый день недели!");
+else if(num == 5) Console.WriteLine("Пятница - это пятый день недели!");
+else if(num == 6) Console.WriteLine("Суббота - это шестой день недели!");
+else if(num == 7) Console.WriteLine("Воскресенье - это седьмой день недели!");
+else Console.WriteLine("Какое большое число - в неделе всего семь дней;)");
+
+
+
+// if(num == 1)
+// {
+//     Console.WriteLine("Понедельник - это первый день недели!");
+// }
+// else
+// {
+//     if(num == 2)
+//     {
+//         Console.WriteLine("Вторник - это второй день недели!");
+//     }
+//     else
+//     {
+//         if(num == 3)
+//         {
+//             Console.WriteLine("Среда - это третий день недели!");
+//         }
+//         else
+//         {
+//             if(num == 4)
+//             {
+//                 Console.WriteLine("Четверг - это четвёртый день недели!");
+//             }
+//             else
+//             {
+//                 if(num == 5)
+//                 {
+//                     Console.WriteLine("Пятница - это пятый день недели!");
+//                 }
+//                 else
+//                 {
+//                     if(num == 6)
+//                     {
+//                         Console.WriteLine("Суббота - это шестой день недели!");
+//                     }
+//                     else{
+//                         if(num == 7)
+//                         {
+//                             Console.WriteLine("Воскресенье - это седьмой день недели!");
+//                         }
+//                         else
+//                         {
+//                             Console.WriteLine("Какое большое число - в неделе всего семь дней;)");
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
 
