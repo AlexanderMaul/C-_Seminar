5f561e3d9ccde4d09d4f902ed0ad1191c93064da
Iskander <iskander-m.s@mail.ru>
2022-07-13 00:01:37 +0700

    Добавил исключения в .gitignore

--- Task_00/Program.cs
+++ Task_00/Program.cs
@@ -1,2 +1,7 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿// 0. Демонстрация решения
+// Написать программу, которая на вход принимает число и выдаёт его квадрат
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int square = num * num;
+Console.WriteLine($"Квадрат числа {num} = {square}");
