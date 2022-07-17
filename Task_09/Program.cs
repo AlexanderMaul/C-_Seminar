1de351805100d25af9a5cdf5be720c76c17a9941
Iskander <iskander-m.s@mail.ru>
2022-07-18 00:45:15 +0700

    Задача 9 решена

--- Task_09/Program.cs
+++ Task_09/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 9. Напишите программу, которая выводит случайное число из отрезка [10.99] и показывает наибольшую цифру числа
+
+int rndNum = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число из отрезка 10 - 99 => {rndNum}");
+
+int firstDigit = rndNum / 10;
+int secondDigit = rndNum % 10;
+int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+Console.WriteLine($"Наибольшая цифра числа {rndNum} => {maxDigit}");
+
+
+
+// Вариант б)
+// int maxDigit = 0;
+// if (firstDigit > secondDigit) maxDigit = firstDigit;
+// else maxDigit = secondDigit;
+// Console.WriteLine($"Наибольшая цифра числа {rndNum} => {maxDigit}");
+
+// Вариант а)
+// if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа {rndNum} => {firstDigit}");
+// else Console.WriteLine($"Наибольшая цифра числа {rndNum} => {secondDigit}");
+
+
