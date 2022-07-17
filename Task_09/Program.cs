2e29107e9d2c5634e8c84f99307e59f99187aed4
Iskander <iskander-m.s@mail.ru>
2022-07-18 01:13:11 +0700

    Задача 9 - добавили метод

--- Task_09/Program.cs
+++ Task_09/Program.cs
@@ -2,21 +2,36 @@
 
 int rndNum = new Random().Next(10, 100);
 Console.WriteLine($"Случайное число из отрезка 10 - 99 => {rndNum}");
+// Вариант г)
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    if (firstDigit == secondDigit) return 0;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
 
-int firstDigit = rndNum / 10;
-int secondDigit = rndNum % 10;
-int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
-Console.WriteLine($"Наибольшая цифра числа {rndNum} => {maxDigit}");
+int maxDigit = MaxDigit (rndNum);
+string result = maxDigit > 0 ? maxDigit.ToString() : "Цифры одинаковые";
+Console.WriteLine($"Наибольшая цифра числа {rndNum} => {result}");
 
-
+// Вариант в)
+// int firstDigit = rndNum / 10;
+// int secondDigit = rndNum % 10;
+// int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+// Console.WriteLine($"Наибольшая цифра числа {rndNum} => {maxDigit}");
 
 // Вариант б)
+// int firstDigit = rndNum / 10;
+// int secondDigit = rndNum % 10;
 // int maxDigit = 0;
 // if (firstDigit > secondDigit) maxDigit = firstDigit;
 // else maxDigit = secondDigit;
 // Console.WriteLine($"Наибольшая цифра числа {rndNum} => {maxDigit}");
 
 // Вариант а)
+// int firstDigit = rndNum / 10;
+// int secondDigit = rndNum % 10;
 // if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа {rndNum} => {firstDigit}");
 // else Console.WriteLine($"Наибольшая цифра числа {rndNum} => {secondDigit}");
 
