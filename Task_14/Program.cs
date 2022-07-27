d7fd25d5915a652a346558735ee735a20a6e96b8
Iskander <iskander-m.s@mail.ru>
2022-07-28 02:58:28 +0700

    Задача 14 решена

--- Task_14/Program.cs
+++ Task_14/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 14. Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+Console.WriteLine("Программа проверки кратности числа одновременно на 7 и на 23");
+Console.WriteLine("Введите число и нажмите Enter");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+string krtn(int num)
+{
+    if (num % 7 == 0 && num % 23 == 00) 
+    {
+        string result = "Да"; 
+        return result;
+    }
+    else
+    {
+        string result = "Нет";
+        return result;
+    }
+}
+
+Console.WriteLine(krtn(numA));
