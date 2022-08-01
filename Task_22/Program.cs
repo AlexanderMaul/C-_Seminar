044cc7a55c62befcaffb9fafedd98f92b7da029c
Iskander <iskander-m.s@mail.ru>
2022-08-01 22:03:12 +0700

    Задача 22 решена

--- Task_22/Program.cs
+++ Task_22/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 22. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N
+
+Console.WriteLine("Программа нахождения квадратов для чисел от 1 до N");
+Console.Write("Введите число N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+// Простой метод
+int index = 1;
+int square = 0;
+while (index <= num)
+{       
+    square = index * index;
+    Console.WriteLine($"{index, 10} -> {square, 10}");
+    index++;
+}
+
+// Массив из семинара
+// int index = 1;
+// int[] tableq = new int [num];
+
+// while (index <= num)
+// {
+// tableq[index - 1] = index * index;
+// index++;
+// }
+
+// Console.WriteLine ($"Таблица квадратов чисел от 1 до {num}: {String.Join(", ",tableq)}");
+
+
+
+
+
+
